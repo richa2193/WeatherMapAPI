576d8bda0467c9017619b26d9d158ec2d7670fce
Richa Parmar <richaparmar0109@gmail.com>
2026-08-29 17:04:25 +0530

    Changes

--- WeatherMapAPI/WeatherMapAPI/settings.py
+++ WeatherMapAPI/WeatherMapAPI/settings.py
@@ -31,14 +31,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = [
-    host.strip()
-    for host in os.environ.get(
-        'ALLOWED_HOSTS',
-        '127.0.0.1,localhost'
-    ).split(',')
-    if host.strip()
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
